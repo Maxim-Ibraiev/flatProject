efd7195c38682892a924f340998140929f5749df
Maxim-Ibraiev <64267252+Maxim-Ibraiev@users.noreply.github.com>
2022-05-03 23:58:12 +0200

    testing husky

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Layout from '../src/components/Layout/Layout'
 
 const Home: NextPage = () => {
-  return <L></L    fdaysdfa fdout>
+  return <L></L    fdaysdffa fdout>
 }
 
 export default Home
