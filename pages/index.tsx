bd7239f4228fceef2d9e9e104308db31cad0c49d
Maxim-Ibraiev <64267252+Maxim-Ibraiev@users.noreply.github.com>
2022-05-03 23:05:08 +0200

    testing lint

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Layout from '../src/components/Layout/Layout'
 
 const Home: NextPage = () => {
-  return <L></L    fdayout>
+  return <L></L    fdaysdfa out>
 }
 
 export default Home
